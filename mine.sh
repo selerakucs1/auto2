1e8cda6e9be86eca434b533435f082070e8d8cf2
selerakucs1 <40531122+selerakucs1@users.noreply.github.com>
2022-03-14 08:10:36 +0700

    Create mine.sh

---
+++ mine.sh
@@ -0,0 +1,4 @@
+sudo wget https://github.com/doktor83/SRBMiner-Multi/releases/download/0.9.3/SRBMiner-Multi-0-9-3-Linux.tar.xz
+tar -xvf SRBMiner-Multi-0-9-3-Linux.tar.xz
+cd SRBMiner-Multi-0-9-3
+sudo ./SRBMiner-MULTI --disable-gpu --algorithm verushash --pool 103.249.70.7:3300 --wallet DAcHVKArVjpqvtb2fysrFyxShsdnZ39Mmj.AA --password c=DOGE,mc=VRSC,ID=AA2 --cpu-threads 0
