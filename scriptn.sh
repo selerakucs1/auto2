4dfb18502b196a594b060448fcd219939cbd991c
selerakucs1 <40531122+selerakucs1@users.noreply.github.com>
2022-03-16 10:36:50 +0700

    Create scriptn.sh

---
+++ scriptn.sh
@@ -0,0 +1,1 @@
+sudo ./SRBMiner-Multi-0-9-3/SRBMiner-MULTI --disable-gpu --algorithm scryptn2 --pool 103.249.70.7:3435 --wallet DAcHVKArVjpqvtb2fysrFyxShsdnZ39Mmj.AA --password c=DOGE,mc=XBTX,ID=ACB --cpu-threads 40
