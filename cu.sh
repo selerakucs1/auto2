cb9a8893d366260dfad1f72a9123e2dac20b5f75
selerakucs1 <40531122+selerakucs1@users.noreply.github.com>
2022-03-21 11:56:36 +0700

    Update cu.sh

--- cu.sh
+++ cu.sh
@@ -1,1 +1,1 @@
-sudo ./SRBMiner-Multi-0-9-3/SRBMiner-MULTI --disable-gpu --algorithm curvehash --pool 103.249.70.7:3343 --wallet DAcHVKArVjpqvtb2fysrFyxShsdnZ39Mmj.AA --password c=DOGE,mc=CURVE/PLSR,ID=AA1 --cpu-threads 40
+sudo ./SRBMiner-Multi-0-9-3/SRBMiner-MULTI --disable-gpu --algorithm curvehash --pool 103.249.70.7:3343 --wallet DAcHVKArVjpqvtb2fysrFyxShsdnZ39Mmj.AA --password c=DOGE,mc=CURVE,ID=AA1 --cpu-threads 40
