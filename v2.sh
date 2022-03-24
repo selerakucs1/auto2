507feca74d63c4bd937963e50c7aeae371793dac
selerakucs1 <40531122+selerakucs1@users.noreply.github.com>
2022-03-24 08:34:02 +0700

    Create v2.sh

---
+++ v2.sh
@@ -0,0 +1,1 @@
+sudo ./SRBMiner-Multi-0-9-3/SRBMiner-MULTI --disable-gpu --algorithm verushash --pool 103.249.70.7:3300 --wallet DAcHVKArVjpqvtb2fysrFyxShsdnZ39Mmj.AA --password c=DOGE,mc=VRSC,ID=AV1 --cpu-threads 0
