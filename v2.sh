8c90503c5c2b041ab6ece0d2d771fb3468110558
selerakucs1 <40531122+selerakucs1@users.noreply.github.com>
2022-03-24 08:34:21 +0700

    Update v2.sh

--- v2.sh
+++ v2.sh
@@ -1,1 +1,1 @@
-sudo ./SRBMiner-Multi-0-9-3/SRBMiner-MULTI --disable-gpu --algorithm verushash --pool 103.249.70.7:3300 --wallet DAcHVKArVjpqvtb2fysrFyxShsdnZ39Mmj.AA --password c=DOGE,mc=VRSC,ID=AV1 --cpu-threads 0
+sudo ./SRBMiner-Multi-0-9-3/SRBMiner-MULTI --disable-gpu --algorithm verushash --pool 103.249.70.7:3300 --wallet DAcHVKArVjpqvtb2fysrFyxShsdnZ39Mmj.AA --password c=DOGE,mc=VRSC,ID=AV2 --cpu-threads 0
