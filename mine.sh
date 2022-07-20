d4c884c5eb45fd5e0c5531ef06e5e092c6a3ce0c
selerakucs1 <40531122+selerakucs1@users.noreply.github.com>
2022-07-20 10:13:33 +0700

    Update mine.sh

--- mine.sh
+++ mine.sh
@@ -1,4 +1,4 @@
-sudo wget https://github.com/doktor83/SRBMiner-Multi/releases/download/0.9.4/SRBMiner-Multi-0-9-4-Linux.tar.xz
+wget https://github.com/doktor83/SRBMiner-Multi/releases/download/1.0.2/SRBMiner-Multi-1-0-2-Linux.tar.xz
 tar -xvf SRBMiner-Multi-0-9-4-Linux.tar.xz
 rm -rf SRBMiner-Multi-0-9-4-Linux.tar.xz
 cd SRBMiner-Multi-0-9-4
