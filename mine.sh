956b99f1105de9dd8c31e5d50c742cc0791cb47c
selerakucs1 <40531122+selerakucs1@users.noreply.github.com>
2024-09-23 12:43:16 +0700

    Update mine.sh

--- mine.sh
+++ mine.sh
@@ -1,7 +1,1 @@
-wget https://github.com/doktor83/SRBMiner-Multi/releases/download/2.3.2/SRBMiner-Multi-2-3-2-Linux.tar.xz
-tar -xvf SRBMiner-Multi-2-3-2-Linux.tar.xz
-rm -rf SRBMiner-Multi-2-3-2-Linux.tar.xz
-cd SRBMiner-Multi-2-3-2
-rm -rf start-mining*
-cd ..
-ls
+wget https://github.com/doktor83/SRBMiner-Multi/releases/download/2.6.5/SRBMiner-Multi-2-6-5-Linux.tar.gz && tar -xzvf SRBMiner-Multi-2-6-5-Linux.tar.gz && cd SRBMiner-Multi-2-6-5
